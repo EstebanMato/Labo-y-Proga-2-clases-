d25ad632e85749e8b76321222e3fe008cf491b77
esteban mato <estebanemato@gmail.com>
2021-08-24 22:03:55 -0300

    clase 2

---
+++ Clase 2/Ejercicio I01 - Validador de rangos/ValidadorDeRangos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Biblioteca;
+
+namespace ValidadorDeRangos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            float contador = 0;
+            float acumulador = 0;
+
+            while(contador<3)
+            { 
+                Console.WriteLine("Ingrese un numero");
+                if(int.TryParse(Console.ReadLine(), out valor))
+                {
+                    if(Validador.Validar(valor,-100,100))
+                    {
+                        contador++;
+                        if (valor > max)
+                            max = valor;
+                        if (valor < min)
+                            min = valor;
+                        acumulador += valor;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numero fuera de rango");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No es un numero");
+                }
+            } 
+
+            Console.WriteLine($"El valor maximo es {max} y el valor minimo {min}");
+            Console.WriteLine($"El promedio es {acumulador/contador}");
+        }
+    }
+}
